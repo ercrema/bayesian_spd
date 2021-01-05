020a3155a5313cd4be36a5eafb23e66b01f83d01
E.R. Crema <erc62@arch.cam.ac.uk>
2021-01-05 09:13:23 +0000

    Updated raw data using a boundary p=0.5

--- runscripts/data_prep.R
+++ runscripts/data_prep.R
@@ -5,8 +5,8 @@ load(here('data','c14data.RData'))
 
 # Calibrate and Pre-Process Data ####
 obs.caldates = calibrate(c14data$CRA,c14data$Error,calCurves = 'intcal20')
-# Consider samples that have a probability mass over 0.8 within 3400 and 1850, ca. #Late Jomon to End of Middle Yayoi. (n=266)
-index = which.CalDates(obs.caldates,BP<3400&BP>1850,p=0.8)
+# Consider samples that have a probability mass over 0.5 within 3400 and 1850, ca. #Late Jomon to End of Middle Yayoi. (n=288)
+index = which.CalDates(obs.caldates,BP<3400&BP>1850,p=0.5)
 obs.caldates = obs.caldates[index]
 # Extract relevant CRA and CRAErrors
 CRA=c14data$CRA[index]
